19f83e4147096c20516932bbbc8ec91e2e5d3e54
Jian Lik Ng <jlik.lik@gmail.com>
2025-07-29 21:12:43 -0700

    Why is rerendering everything...

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 	// define callbacks
 	http.HandleFunc("/", indexHandler)
 
+	http.HandleFunc("/home-content", homeHandler)
+	http.HandleFunc("/brand-content", brandHandler)
+
 	// then listen and serve
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -24,10 +27,30 @@ func main() {
 }
 
 // See https://pkg.go.dev/net/http#example-HandleFunc for examples
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w htp.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Unable to load index.html", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("home.html")
+	if err != nil {
+		http.Error(w, "Unable to load home.html", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func brandHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("brand.html")
+	if err != nil {
+		http.Error(w, "Unable to load brand.html", http.StatusInternalServerError)
 		return
 	}
 
