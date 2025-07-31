82944647171f71e3e54e0e1de46e4367281f0a14
Jian Lik Ng <jlik.lik@gmail.com>
2025-07-30 21:53:05 -0700

    Start writing home page

--- run_server.sh
+++ run_server.sh
@@ -5,6 +5,13 @@ PORT=8080
 while true; do
 	clear
 
+	# Kill by port
+	PORT=8080
+	PID=$(lsof -ti tcp:$PORT)
+	if [[ -n "$PID" ]]; then
+  	kill $PID
+	fi
+
 	echo "Running app..."
 	go build -o htmx_app .
 	./htmx_app &
